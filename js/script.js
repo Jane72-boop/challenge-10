9e10b2309146b215c9fd099a57d16aea1c580f2e
Jane72-boop <71617423+Jane72-boop@users.noreply.github.com>
2021-12-23 12:35:05 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,85 +4,106 @@ const email = document.getElementById('email')
 const password = document.getElementById('password')
 const claimBtn = document.getElementById('claimBtn')
 
-const firstNameErrTxt = document.createTextNode("First Name cannot be empty");
-const lastNameErrTxt = document.createTextNode("Last Name cannot be empty");
-const emailErrTxt = document.createTextNode("Looks like this is not an email");
-const passErrTxt = document.createTextNode("Not a valid password");
-const passErrTxt0 = document.createTextNode("Password cannot be empty");
-
-
 claimBtn.addEventListener("click", validateInput)
 
-
-function validateInput(){
-    if( validateFName() || validateLName() || validateEmail() || validatePass() === false) {
-        event.preventDefault()
-    } else {
-        !event.preventDefault()
+class Error {
+    constructor (classErr, textErr){
+        this.classErr = classErr
+        this.textErr = textErr
     }
-    
-}
 
-function validateFName() {
-    if (firstName.value === "") {
+    static createErrorElement(text) {
         let p = document.createElement('p')
-        p.appendChild(firstNameErrTxt)
+        p.textContent = text
         p.classList.add('errorTxt')
 
-        firstName.classList.add('errorInput')
-        firstName.parentElement.appendChild(p)
-        return(false)
+        return p 
+    }
+
+    update() {
+        this.classErr.classList.add('errorInput')
+        this.classErr.parentElement.appendChild(Error.createErrorElement(this.textErr))
+    }
+
+    
+
+}
+
+
+function validateInput() {
+    // validateFName()
+    // validateLName()
+    // validateEmail()
+    // validatePassword()
+
+   if(validateFName() || validateLName() || validateEmail() || validatePassword() ===false) { 
+       event.preventDefault() 
+    } else {
+        location.reload()
+    }
+}
+
+
+function validateFName() {
+    if (!firstName.value) { 
+        if(firstName.classList != "errorInput"){
+            let errFirstN = new Error(firstName, "First Name cannot be empty");
+            errFirstN.update()
+        
+            return false
+        }
+        return false
+    }
+    else { 
+        return true 
     }
 }
 
 function validateLName() {
-    if (lastName.value === "") {
-        let p = document.createElement('p')
-        p.appendChild(lastNameErrTxt)
-        p.classList.add('errorTxt')
-
-        lastName.classList.add('errorInput')
-        lastName.parentElement.appendChild(p)
-        return(false)
+    if (!lastName.value) { 
+        if(lastName.classList != "errorInput") {
+            let errLastN = new Error(lastName, "Last Name cannot be empty");
+            errLastN.update()
+            
+            return false
+        }
+        return false
+    
+    } else { 
+        return true 
     }
 }
 
+function validateEmail() {
+    let checkEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)
 
-function validateEmail(){
-
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
-        return(true)
-    } else {
-            let p = document.createElement('p')
-            p.appendChild(emailErrTxt)
-            p.classList.add('errorTxt')
-
-            email.classList.add('errorInput')
-            email.parentElement.appendChild(p)
-            return(false)
+    if (!checkEmail) { 
+        if (email.classList != "errorInput") {
+            let errEmail = new Error(email, "Looks like this is not an email");
+            errEmail.update()
+            
+            return false
         }
+        return false
+    } else { 
+        return true 
+    }
 }
 
-function validatePass() {
+function validatePassword() {
+    let checkPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/.test(password.value)
 
-    if (/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/.test(password.value)) {
-        return(true)
-    } else if(password.value === '') {
-        let p = document.createElement('p')
-        p.appendChild(passErrTxt0)
-        p.classList.add('errorTxt')
-
-        password.classList.add('errorInput')
-        password.parentElement.appendChild(p)
-        return(false)
-
-    } else {
-            let p = document.createElement('p')
-            p.appendChild(passErrTxt)
-            p.classList.add('errorTxt')
-
-            password.classList.add('errorInput')
-            password.parentElement.appendChild(p)
-            return(false)
+    if (!checkPass) { 
+        if(password.classList != "errorInput") {
+            let errPass = new Error(password, "Password cannot be empty");
+            errPass.update()
+            
+            return false 
         }
+        return false
+    } else { 
+        return true 
+    }
 }
+
+//ADD REMOVE ERROR FEATURE
